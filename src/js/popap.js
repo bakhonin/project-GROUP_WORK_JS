609514376974a747cd1df36c37dbdb075e1155c8
klimukviktori@gmail.com <klimukviktori@gmail.com>
2023-12-24 01:37:07 +0200

    something is going to be ok

--- src/js/popap.js
+++ src/js/popap.js
@@ -1,47 +1,56 @@
+import{fetchProducts} from './fetch'
 
-    //     return arr.map(({ price, name, img, _id, category, size, popularity }) =>
-    //     ` <div class="popup-content" id="${_id}">
-    //       <button type="button" class="popup-close close-popup btn-close-modal">
-    //           <svg class="modal-btn" width="8" heigth="8">
-    //              <use href="../img/sprite.svg#icon-closed"></use>
-    //           </svg>
-    //       </button>
-    //       <div class="block">
-    //         <img width=100 src="${img}">
-    //       </div>
-    //       <div class="info">
-    //          <h3 class="info-title">${name}</h3>
-    //          <div class="info-block">
-    //             <div class="info-block-div_first">
-    //                <h5 class="info-block-div-h5">Category<span class="info-block-div-span">${category}</span></h5>
-    //                <h5 class="info-block-div-h5">Size<span class="info-block-div-span">${size}</span></h5>
-    //             </div>
-    //             <div class="info-block-div_sec">
-    //                <h5 class="info-block-div-h5">Popular<span class="info-block-div-span">${popularity}</span></h5>
-    //             </div>
-    //             <p class="inf0-block-p">A round, orange vegetable with a thick shell and seeds inside. It is often used in cooking and baking for its sweet and earthy flavor.</p>
-    //             <div class="info-block-div_price">
-    //                <h5 class="info-block-price">${price}</h5>
-    //                <button type="button " class="btn-styles btn_add">Add to
-    //                   <svg width="14" height="14" > 
-    //                      <use href="./img/sprite.svg#icon-basket" > </use > 
-    //                   </svg >
-    //                </button>
-    //             </div>
-    //          </div>
-    //       </div>
-    //      </div>
-    //     `
-    // ).join('')
-      import { fetchProducts } from "./fetch";
+// console.log(5); //то була перевірка чи працює файл
+const container = document.querySelector(".popup");
+console.log(container);
+const item = document.querySelector(".products-container ");
+console.log(item);
+// Те що в комментах не працює. Я хочу спитати в тих хто робив файл templates.js. як то зробити.
+// container.insertAdjacentHTML("beforeend", createMarkUpCard(fetchProducts));
 
-    //   async function renderAll() {
-    //     try {
-    //         const data = await fetchProductsAll('Fresh_Produce')
-    //         containerAll.insertAdjacentHTML("beforeend", createMarkupProductsAll(data))
-    //     } catch (error) {
-    //         console.log(error.message)
-    //     }
-    // }
-    // renderAll()
+// function createMarkUpCard(fetchProducts) {
+//     return fetchProducts.map(({price, name, img, _id, category, size, popularity, desc}) =>
+//     `<div class="popup-body" id="${_id}">
+//         <div class="popup-content">
+//             <button type="button" class="popup-close close-popup" id="closeModal">
+//                <svg class="btn-close">
+//                    <use href="../img/sprite.svg#icon-closed"></use>
+//                </svg>
+//             </button> 
+//             <div class="popup-block">
+//                <div class="popup-block-img">
+//                   <img src="${img}" alt="${name}" srcset="" class="imgId">
+//                </div>
+//                <h3 class="popup-block-title">${name}</h3>
+//                <div class="popup-block-info">
+//                   <div class="order">
+//                      <h5 class="popup-block-info-param">Category:<span class="info-param-value">${category}</span></h5>
+//                      <h5 class="popup-block-info-param">Size:<span class="info-param-value">${size}</span></h5>
+//                   </div>
+//                   <h5 class="popup-block-info-param">Popularity:<span class="info-param-value">${popularity}</span></h5>
+//                </div>
+//                <p class="popup-block-p">${desc}</p>
+//                <div class="popup-block-price">
+//                   <h3 class="popup-block-price-h3">${price}</h3>
+//                   <button type="button" class="btn-styles">Add to
+//                      <svg width="14" height="14" > 
+//                         <use href="./img/sprite.svg#icon-basket" > </use > 
+//                      </svg >
+//                   </button>
+//                </div>
+//             </div>
+//         </div> 
+//     </div>`).join("")
+// }
+// 
+item.addEventListener("click", handleClick);
+function handleClick(event) {
+    console.log(event.target);
+    if (event.target === event.currentTarget) {
+        return
+    }
+
+    container.style.display = 'flex'
+
+}
 
